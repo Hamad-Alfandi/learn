f1b88d682d27b6745aa48dd3e0c6848ce9366fe1
Hamad-Alfandi <Hamad.alfandi7@gmail.com>
2025-02-20 16:02:19 +0300

    Initial commit - squashed history

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String []args) {
+        System.out.println("Hello, World!"); // prints Hello, World!
+        System.out.println("Hello, World! 2"); // prints Hello, World!
+        System.out.println("Hello, World! 3"); // prints Hello, World!
+        System.out.println("Hello, World! 4"); // prints Hello, World!
+        System.out.println("Hello, World! 5"); // prints Hello, World!
+
+    }
+}
